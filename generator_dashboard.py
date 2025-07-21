97170519942965e568cd29f0e79d305f7c796ce8
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-07-21 11:59:23 +0300

    am adaugat try

--- generator_dashboard.py
+++ generator_dashboard.py
@@ -4,14 +4,23 @@ import pandas as pd
 
 st.title("DashBoard Generator")
 st.markdown("<h3 style='font-size:24px'>Incarcati un fisier de date</h3>",unsafe_allow_html=True)
-fisiere_de_incarcat=st.file_uploader(" ",type=["csv"])
+fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
+
+
 def procesare_fisier():
- if fisiere_de_incarcat is not None :
-    df=pd.read_csv(fisiere_de_incarcat)
-    st.write("Datele din fisiere")
-    st.dataframe(df)
-    
- else:
-    st.write("Fisierul nu s-a incarcat!")
+ try:
+   if fisiere_de_incarcat is not None :
+     if fisiere_de_incarcat.type()=="json":
+       df=pd.read_json(fisiere_de_incarcat)
+       st.write("Datele din fisiere")
+       st.dataframe(df)
+     else :
+       df=pd.read_csv(fisiere_de_incarcat)
+       st.write("Datele din fisiere")
+       st.dataframe(df)
+   else:
+     st.write("Astept incarcarea fisierului")
+ except Exception as e :
+   st.error(f"Eroare la incarcarea fisierului!{e}")
     
 procesare_fisier();
