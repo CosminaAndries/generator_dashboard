7a83f7d12edf26d08de4c788115a26d5b90168c5
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-07-21 11:27:50 +0300

    am adaugat:

--- generator_dashboard.py
+++ generator_dashboard.py
@@ -1,13 +1,17 @@
 
 import streamlit as st 
+import pandas as pd
 
 st.title("DashBoard Generator")
 st.markdown("<h3 style='font-size:24px'>Incarcati un fisier de date</h3>",unsafe_allow_html=True)
 fisiere_de_incarcat=st.file_uploader(" ",type=["csv"])
-if fisiere_de_incarcat is not None :
+def procesare_fisier():
+ if fisiere_de_incarcat is not None :
     df=pd.read_csv(fisiere_de_incarcat)
     st.write("Datele din fisiere")
     st.dataframe(df)
     
-else
+ else:
     st.write("Fisierul nu s-a incarcat!")
+    
+procesare_fisier();
