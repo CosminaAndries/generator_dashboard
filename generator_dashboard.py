3f233019788cfbcd0fbf2483958428ec9423bc59
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-07-21 12:03:34 +0300

    am modificat tip de fisier

--- generator_dashboard.py
+++ generator_dashboard.py
@@ -10,7 +10,7 @@ fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
 def procesare_fisier():
  try:
    if fisiere_de_incarcat is not None :
-     if fisiere_de_incarcat.type()=="json":
+     if fisiere_de_incarcat.name.endswith(".json"):
        df=pd.read_json(fisiere_de_incarcat)
        st.write("Datele din fisiere")
        st.dataframe(df)
