f727ce29fd1c1c3c94ac9494289e3df8f1195a74
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-11 21:45:09 +0300

    am adaugat iconitele

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Multiapp:
         menu_title="Menu",
         menu_icon="menu.png",
         options=['Home','Cleaning','Visualization'],     
-        icons=['house','broom','bar-chart'],
+        icons=[st.image('house.png'),st.image('broom.png'),st.image('bar-chart.png')],
         styles={
           #"icon":{ "color":"white", "font-size":"23px"},
           "nav-link":{"color":"white", "font-size":"23px","text-align":"left","--hover-color":"blue"},
