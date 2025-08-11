1951186f88825b784d22e68dc978e91a5b941ed5
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-11 18:17:36 +0300

    w

--- generator_dashboard.py
+++ generator_dashboard.py
@@ -7,7 +7,7 @@ st.title("DashBoard Generator")
 st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
 fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
 
-
+page=st.sidebar.radio("Navigare",["DataCleaning","Visualization"])
 def procesare_fisier():
  try:
    if fisiere_de_incarcat is not None :
@@ -24,9 +24,10 @@ def procesare_fisier():
  except Exception as e :
    st.error(f"Eroare la incarcarea fisierului!{e}")
     
-procesare_fisier();
-page=st.sidebar.radio("Navigare",["DataCleaning","Visualization"])
-if page=="Visualizations":
+    
+ if page=="Visualizations":
+  st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
+  fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
  choice_of_which_chart_to_use=st.selectbox('What chart do you want your data to be displayed with?',('None','Bar Chart','Line Chart','Area Chart','Map Chart','Scatterplot Chart','Histograms','Pie Chart'))
  if choice_of_which_chart_to_use=='Bar Chart' :
     if st.session_state.data is not None:
