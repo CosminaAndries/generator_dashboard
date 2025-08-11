7cbd5beb0da2aec073cd12a446198755766eeea7
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 00:52:56 +0300

    schimbare din string in float

--- visualization_pane.py
+++ visualization_pane.py
@@ -82,7 +82,7 @@ def app():
     latitude=st.selectbox("Latitude:",options=df.columns)
     longitude=st.selectbox("Longitude:",options=df.columns)
     if latitude and  longitude  and latitude in df.columns and longitude in df.columns :
-      data=df[[latitude,longitude]].copy()
+      data=float(df[[latitude,longitude]].copy())
       data.columns=['lat','lon']
       grafic=st.map(data)
       fn='map.png'
