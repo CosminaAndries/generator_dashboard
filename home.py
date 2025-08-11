cae7c518851914cc1168d80dea74ab2817c042c3
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-11 22:25:55 +0300

    s

--- home.py
+++ home.py
@@ -7,5 +7,26 @@ from streamlit_option_menu import  option_menu
 def app():
  st.title("DashBoard Generator")
  st.markdown("<h3 style='font-size:24px'>Welcome to my app! </h3>",unsafe_allow_html=True)
-
+ st.title("DashBoard Generator")
+ st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
+ fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
+ def procesare_fisier():
+  try:
+   if fisiere_de_incarcat is not None :
+     if fisiere_de_incarcat.name.endswith(".json"):
+       df=pd.read_json(fisiere_de_incarcat)
+       st.write("The data found in the files")
+       st.session_state.data=df
+       st.dataframe(df)
+     else :
+       df=pd.read_csv(fisiere_de_incarcat)
+       st.write("The data found in the files")
+       st.session_state.data=df
+       st.dataframe(df)
+  except Exception as e :
+   st.error(f"Eroare la incarcarea fisierului!{e}")
+  
+st.set_page_config(
+  page_title="Home Page"
+)
 
--- main.py
+++ main.py
@@ -5,28 +5,7 @@ import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 import home, data_cleaning_pane,visualization_pane
 
-st.title("DashBoard Generator")
-st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
-fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
-def procesare_fisier():
-  try:
-   if fisiere_de_incarcat is not None :
-     if fisiere_de_incarcat.name.endswith(".json"):
-       df=pd.read_json(fisiere_de_incarcat)
-       st.write("The data found in the files")
-       st.session_state.data=df
-       st.dataframe(df)
-     else :
-       df=pd.read_csv(fisiere_de_incarcat)
-       st.write("The data found in the files")
-       st.session_state.data=df
-       st.dataframe(df)
-  except Exception as e :
-   st.error(f"Eroare la incarcarea fisierului!{e}")
-  
-st.set_page_config(
-  page_title="Home Page"
-)
+
 class Multiapp:
   def __init__(self):
     self.apps=[]   
