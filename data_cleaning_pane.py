538f16ddb9fb9235e7a5e0c818049b52e57dd86f
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 12:03:45 +0300

    3

--- data_cleaning_pane.py
+++ data_cleaning_pane.py
@@ -1,2 +1,24 @@
 import streamlit as st
+import pandas as pd
 
+def procesare_fisier(fisiere_de_incarcat):
+   try:
+    if fisiere_de_incarcat is not None :
+     if fisiere_de_incarcat.name.endswith(".json"):
+       df=pd.read_json(fisiere_de_incarcat)
+       st.write("The data found in the files")
+       st.session_state.data=df
+       st.dataframe(df)
+     else :
+       df=pd.read_csv(fisiere_de_incarcat)
+       st.write("The data found in the files")
+       st.session_state.data=df
+       st.dataframe(df)
+
+   except Exception as e :
+    st.error(f"Eroare la incarcarea fisierului!{e}")
+
+def app():
+  st.title("Data Cleaning Page")
+  procesare_fisier()
+  
