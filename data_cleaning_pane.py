35868b305de582d66eab5ead0bbffcb2dbf5b302
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 14:59:26 +0300

    s

--- data_cleaning_pane.py
+++ data_cleaning_pane.py
@@ -17,9 +17,28 @@ def procesare_fisier(fisiere_de_incarcat):
 
    except Exception as e :
     st.error(f"Eroare la incarcarea fisierului!{e}")
+coloane_valori_lipsa=[]
+def verificare_coloane_null(df):
+ for col in df.columns:
+   numar=df[col].is_null.sum()
+   procentaj=(numar/df.shape[1])*100
+   if numar>0 :
+     coloane_valori_lipsa.apend({
+    "numecoloana":df[col],
+    "numar valori lipsa":numar,
+    "procentaj":procentaj})
+ st.write(coloane_valori_lipsa)
+ 
+     
+    
 
 def app():
   st.title("Data Cleaning Page")
   fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
   procesare_fisier(fisiere_de_incarcat)
-  
+  df=st.session_state.data
+  verificare_coloane_null(df)
+  if coloane_valori_lipsa is not None:
+    preferinta=st.select_box("How do you want to handle the missing data",options=("Remove Columns","Replace with mean "))
+  # if preferinta=="Remove Columns" :
+      
