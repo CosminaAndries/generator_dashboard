afeb2d097badd9017dd3a887b27c19d84adb814a
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 11:04:10 +0300

    #7

--- visualization_pane.py
+++ visualization_pane.py
@@ -90,7 +90,7 @@ def app():
       color=st.text_input('If you want collor you can only use RGA color code or HEX code:')
       if color=="None"or color=="No":
        color="#d59a75"
-       plt.scatter(x=x,y=y,c=color )
+       plt.scatter(x=df[x],y=df[y],c=color )
        st.pyplot(plt)
        fn='scatter.png'
        plt.savefig(fn)
