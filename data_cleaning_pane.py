83f419554ea3202fb4fd3af7d0862bc6aa13edf6
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 16:02:21 +0300

    test2

--- data_cleaning_pane.py
+++ data_cleaning_pane.py
@@ -45,8 +45,8 @@ def app():
     numerice=df.select_dtypes(include=['number']).columns.tolist()
     textuale=df.select_dtypes(include=['object','string']).columns.tolist()
     if numerice and textuale  is not None:
-     preferinta_num=st.selectbox("How do you want to handle the missing data",options=["Remove Columns","Replace with mean ","Replace with median"])
-     preferinta_text=st.selectbox("How do you want to handle the missing data",options=["Remove Columns","Replace with mode ", "Replace with 'Unknown'"])
+     preferinta_num=st.selectbox("How do you want to handle the missing data",options=["None","Remove Columns","Replace with mean ","Replace with median"])
+     preferinta_text=st.selectbox("How do you want to handle the missing data",options=["None","Remove Columns","Replace with mode ", "Replace with 'Unknown'"])
     if preferinta_num=="Remove Columns":
       df.dropna(axis=1)
       st.dataframe(df)
