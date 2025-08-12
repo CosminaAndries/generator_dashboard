000a4d95ba4361517721b8907e72bcd5a70c5d73
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-12 14:07:19 +0300

    am adaugat optiune de descriere a datelor

--- home.py
+++ home.py
@@ -4,14 +4,7 @@ import altair as alt
 import matplotlib.pyplot as plt
 from streamlit_option_menu import  option_menu
 
-st.set_page_config(
-  page_title="Home Page"
-)
-def app():
- st.title("DashBoard Generator")
- st.markdown("<h3 style='font-size:24px'>Welcome to my app!</h3>",unsafe_allow_html=True)
- fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
- def procesare_fisier():
+def procesare_fisier( fisiere_de_incarcat):
   try:
    if fisiere_de_incarcat is not None :
      if fisiere_de_incarcat.name.endswith(".json"):
@@ -26,6 +19,28 @@ def app():
        st.dataframe(df)
   except Exception as e :
    st.error(f"Eroare la incarcarea fisierului!{e}")
+   
+def informatii_despre_date(df):
+ st.markdown("<h3 style='font-size:23px'>Data Shape</h3>",unsafe_allow_html=True)
+ st.write(df.shape)
+ st.markdown("<h3 style='font-size:23px'>Data Description</h3>",unsafe_allow_html=True)
+ st.write(df.describe())
+ st.markdown("<h3 style='font-size:23px'>Columns Name</h3>",unsafe_allow_html=True)
+ st.write(df.columns)
+ 
+
+def app():
+ st.title("DashBoard Generator")
+ st.markdown("<h3 style='font-size:24px'>Welcome to my app!</h3>",unsafe_allow_html=True)
+ fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
+ procesare_fisier(fisiere_de_incarcat)
+ df=st.session_state.data
+ df=pd.DataFrame(df)
+ st.markdown("<h3 style='font-size:24px'> Information about the data!</h3>",unsafe_allow_html=True)
+ informatii_despre_date(df)
+
+
+    
   
 
 
