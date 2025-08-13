252b0c265b683246a561910684a6e50cb580c7e4
CosminaAndries <cosmina.gabriela.andries@gmail.com>
2025-08-13 14:47:25 +0300

    am adaugat

--- generator_dashboard.py
+++ generator_dashboard.py
@@ -3,35 +3,9 @@ import pandas as pd
 import altair as alt
 import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
-import visualization_pane, data_cleaning_pane
+import home, data_cleaning_pane,visualization_pane
 
-st.title("DashBoard Generator")
-st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
-fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
 
-#page=st.sidebar.radio("Navigare",['Main',"DataCleaning","Visualization"])
-def procesare_fisier():
-  try:
-   if fisiere_de_incarcat is not None :
-     if fisiere_de_incarcat.name.endswith(".json"):
-       df=pd.read_json(fisiere_de_incarcat)
-       st.write("The data found in the files")
-       st.session_state.data=df
-       st.dataframe(df)
-     else :
-       df=pd.read_csv(fisiere_de_incarcat)
-       st.write("The data found in the files")
-       st.session_state.data=df
-       st.dataframe(df)
-  except Exception as e :
-   st.error(f"Eroare la incarcarea fisierului!{e}")
-  
-st.markdown("<h3 style='font-size:24px'>Upload a date file:</h3>",unsafe_allow_html=True)
-fisiere_de_incarcat=st.file_uploader(" ",type=["csv","json"])
-visualize_button=st.clean_button("Visualize Data")
-st.set_page_config(
-  page_title="Home Page"
-)
 class Multiapp:
   def __init__(self):
     self.apps=[]   
@@ -40,20 +14,34 @@ class Multiapp:
       "title":title,
       "function":function
     })
-  def run():
+  def run(self):
      with st.sidebar:
       app=option_menu(
         menu_title="Menu",
+        menu_icon="list",
         options=['Home','Cleaning','Visualization'],     
-        icons=['home.png','broom.png','bar-chart.png'],
+        icons=['house','trash' ,'table'],
         styles={
           "icon":{ "color":"white", "font-size":"23px"},
-          "nav-link":{"color":"white", "font-size":"23px","text-align":"left","--hover-color":"blue"},
+          "nav-link":{"color":"white", "font-size":"20px","text-align":"left","margin":"0px","--hover-color":"blue"},
           "nav-link-selected":{"background-color":"#02ab21"},
           }
       )
-      if app=="Home":
-        home.run()
-      
+     if app=="Home":
+       st.set_page_config(
+         page_title="Home Page"
+            )
+       home.app()
+     elif app=="Visualization":
+        st.set_page_config(
+         page_title="Visualization Page"
+            )
+        visualization_pane.app()
+     elif app=="Cleaning":
+        st.set_page_config(
+         page_title="Data Cleaning Page"
+            )
+        data_cleaning_pane.app()
     
-      
+app = Multiapp()
+app.run()
